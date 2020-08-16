ab3e45a298cbf489638cc7c57a15a0154fc914af
Tumy <brad.tumy@etrade.com>
2020-08-15 21:00:21 -0400

    updated server.go with replacing example code

--- server.go
+++ server.go
@@ -1,77 +1,149 @@
-package main 
+package main
 
 import (
     "log"
-	"net/http"
-	"encoding/json"
-	"fmt"
-	"github.com/google/uuid"
-	//"time"
+    "net/http"
+    "net/url"
+    "os"
 
-	"gopkg.in/oauth2.v3/models"
-	"gopkg.in/oauth2.v3/errors"
-	"gopkg.in/oauth2.v3/manage"
-	"gopkg.in/oauth2.v3/server"
-	"gopkg.in/oauth2.v3/store"
+    "github.com/go-session/session"
+    "gopkg.in/oauth2.v3/errors"
+    "gopkg.in/oauth2.v3/manage"
+    "gopkg.in/oauth2.v3/models"
+    "gopkg.in/oauth2.v3/server"
+    "gopkg.in/oauth2.v3/store"
 )
 
 func main() {
-	manager := manage.NewDefaultManager()
-	manager.SetAuthorizeCodeTokenCfg(manage.DefaultAuthorizeCodeTokenCfg)
+    manager := manage.NewDefaultManager()
+    // token store
+    manager.MustTokenStorage(store.NewMemoryTokenStore())
 
-	manager.MustTokenStorage(store.NewMemoryTokenStore())
+    clientStore := store.NewClientStore()
+    clientStore.Set("222222", &models.Client{
+        ID:     "222222",
+        Secret: "22222222",
+        Domain: "http://localhost:9094",
+    })
+    manager.MapClientStorage(clientStore)
 
+    srv := server.NewServer(server.NewConfig(), manager)
+    srv.SetUserAuthorizationHandler(userAuthorizeHandler)
 
-	clientStore := store.NewClientStore()
-	manager.MapClientStorage(clientStore)
+    srv.SetInternalErrorHandler(func(err error) (re *errors.Response) {
+        log.Println("Internal Error:", err.Error())
+        return
+    })
 
-	srv := server.NewDefaultServer(manager)
-	srv.SetAllowGetAccessRequest(true)
-	srv.SetClientInfoHandler(server.ClientFormHandler)
-	manager.SetRefreshTokenCfg(manage.DefaultRefreshTokenCfg)
+    srv.SetResponseErrorHandler(func(re *errors.Response) {
+        log.Println("Response Error:", re.Error.Error())
+    })
 
-	srv.SetInternalErrorHandler(func(err error) (re *errors.Response) {
-		log.Println("Internal Errors:", err.Error())
-		return
-	})
+    http.HandleFunc("/login", loginHandler)
+    http.HandleFunc("/auth", authHandler)
 
-	srv.SetResponseErrorHandler(func(re *errors.Response) {
-		log.Println("Response Error:", re.Error.Error())
-	})
+    http.HandleFunc("/authorize", func(w http.ResponseWriter, r *http.Request) {
+        err := srv.HandleAuthorizeRequest(w, r)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusBadRequest)
+        }
+    })
 
-    http.HandleFunc("/protected", validateToken(func(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte("Hello, I'm protected"))
-	}, srv))
-	
-	http.HandleFunc("/credentials", func(w http.ResponseWriter, r *http.Request) {
-		clientId := uuid.New().String()[:8]
-		clientSecret := uuid.New().String()[:8]
-		err := clientStore.Set(clientId, &models.Client{
-			ID: clientId,
-			Secret: clientSecret,
-			Domain: "http://localhost:9094",
-	})
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(map[string]string{"ClIENT_ID": clientId, "CLIENT_SECRET":clientSecret})
-	})
+    http.HandleFunc("/token", func(w http.ResponseWriter, r *http.Request) {
+        err := srv.HandleTokenRequest(w, r)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+        }
+    })
 
-	http.HandleFunc("/token", func(w http.ResponseWriter, r *http.Request) {
-		srv.HandleTokenRequest(w, r)
-	})
-
-	log.Fatal(http.ListenAndServe(":9096", nil))
+    log.Println("Server is running at 9096 port.")
+    log.Fatal(http.ListenAndServe(":9096", nil))
 }
 
-func validateToken(f http.HandlerFunc, srv *server.Server) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-		_, err := srv.ValidationBearerToken(r)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-		f.ServeHTTP(w, r)
-	})
+func userAuthorizeHandler(w http.ResponseWriter, r *http.Request) (userID string, err error) {
+    store, err := session.Start(nil, w, r)
+    if err != nil {
+        return
+    }
+
+    uid, ok := store.Get("UserID")
+    if !ok {
+        if r.Form == nil {
+            r.ParseForm()
+        }
+        store.Set("ReturnUri", r.Form)
+        store.Save()
+
+        w.Header().Set("Location", "/login")
+        w.WriteHeader(http.StatusFound)
+        return
+    }
+    userID = uid.(string)
+    store.Delete("UserID")
+    store.Save()
+    return
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+    store, err := session.Start(nil, w, r)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    if r.Method == "POST" {
+        store.Set("LoggedInUserID", "000000")
+        store.Save()
+
+        w.Header().Set("Location", "/auth")
+        w.WriteHeader(http.StatusFound)
+        return
+    }
+    outputHTML(w, r, "static/login.html")
+}
+
+func authHandler(w http.ResponseWriter, r *http.Request) {
+    store, err := session.Start(nil, w, r)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    if _, ok := store.Get("LoggedInUserID"); !ok {
+        w.Header().Set("Location", "/login")
+        w.WriteHeader(http.StatusFound)
+        return
+    }
+
+    if r.Method == "POST" {
+        var form url.Values
+        if v, ok := store.Get("ReturnUri"); ok {
+            form = v.(url.Values)
+        }
+        u := new(url.URL)
+        u.Path = "/authorize"
+        u.RawQuery = form.Encode()
+        w.Header().Set("Location", u.String())
+        w.WriteHeader(http.StatusFound)
+        store.Delete("Form")
+
+        if v, ok := store.Get("LoggedInUserID"); ok {
+            store.Set("UserID", v)
+        }
+        store.Save()
+
+        return
+    }
+    outputHTML(w, r, "static/auth.html")
+}
+
+func outputHTML(w http.ResponseWriter, req *http.Request, filename string) {
+    file, err := os.Open(filename)
+    if err != nil {
+        http.Error(w, err.Error(), 500)
+        return
+    }
+    defer file.Close()
+    fi, _ := file.Stat()
+    http.ServeContent(w, req, file.Name(), fi.ModTime(), file)
 }
